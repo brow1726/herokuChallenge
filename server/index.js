0ab13f115fdaaf07a15cd6634b4b3c137f97f217
Matthew Brown <MatthewBrown@Matthews-MacBook-Air.local>
2015-08-25 16:43:59 -0500

    working now

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 var server = app.listen(process.env.PORT || 3000, function(){
     var port = server.address().port;
