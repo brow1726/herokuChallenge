68b6725032754b8de9cd8f71af80555652ef37a3
Matthew Brown <MatthewBrown@Matthews-MacBook-Air.local>
2015-08-25 16:29:53 -0500

    configured server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
-app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname, './public'));
 
 var server = app.listen(process.env.PORT || 3000, function(){
     var port = server.address().port;
