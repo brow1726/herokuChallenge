7e40ac65773e2aaa5c1bb23dd2cd6026819f7b11
Matthew Brown <MatthewBrown@Matthews-MacBook-Air.local>
2015-08-26 09:30:48 -0500

    changed folder name

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ var path = require('path');
 var app = express();
 
 
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, '../pubicle')));
 
 var server = app.listen(process.env.PORT || 3000, function(){
     var port = server.address().port;
